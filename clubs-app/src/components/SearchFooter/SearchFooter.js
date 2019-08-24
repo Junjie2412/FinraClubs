8538e0feb8ad1941ac72c073299af7e3f5c6a38f
Wang <K28184@corp.root.nasd.com>
2019-08-24 18:22:48 -0400

    commit

--- clubs-app/src/components/SearchFooter/SearchFooter.js
+++ clubs-app/src/components/SearchFooter/SearchFooter.js
@@ -10,7 +10,7 @@ const SearchFooter = (props) => {
             <h1 className={classes.Header}>{props.header}</h1>
             <i className={["fa fa-search", classes.searchButton].join(' ')}></i>
             <input type={"text"} className={classes.Search} placeholder={'Search by Category'}/>
-            
+
         </div>
 
     );
