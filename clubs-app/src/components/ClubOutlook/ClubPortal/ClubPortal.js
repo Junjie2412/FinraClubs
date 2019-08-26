adf47af1083c23484ea5ffa36f8c73c64c3f0da2
Junjie Wang <junjie2412@gmail.com>
2019-08-26 00:10:15 -0400

    stuffs

--- clubs-app/src/components/ClubOutlook/ClubPortal/ClubPortal.js
+++ clubs-app/src/components/ClubOutlook/ClubPortal/ClubPortal.js
@@ -6,9 +6,9 @@ const ClubPortal = (props, context) => {
 
     //non members - see apply and the top div
     return (
-        <NavLink to={"/clubpage/"+props.name}>
-            <div className={props.small ? classes.ClubPortalSmall : classes.ClubPortal}>
-                {/*}
+        <NavLink to={"/clubpage/"+props.name} style={{"pointer-events":props.disable ? "none" : ""}}>
+            <div className={[props.small ? classes.ClubPortalSmall : classes.ClubPortal,props.full ? classes.Full: ""].join(" ")}>
+                {/*
                 <div className={classes.overlay}>
                     <NavLink to={"/clubpage/"+props.name} className={classes.overlayLink}>
                         <button className={classes.overlayButton}>
@@ -19,7 +19,8 @@ const ClubPortal = (props, context) => {
                 <div className={classes.imageContainer}>
                     <img className={classes.image} alt={''} src={props.image}/>
                 </div>
-                <h3 className={props.small ? classes.HeaderSmall : classes.Header}>
+                <h3 className={[props.small ? classes.HeaderSmall : classes.Header,
+                                props.full ? classes.HeaderFull: ""].join(' ')}>
                     {props.name}
                 </h3>
             </div>
